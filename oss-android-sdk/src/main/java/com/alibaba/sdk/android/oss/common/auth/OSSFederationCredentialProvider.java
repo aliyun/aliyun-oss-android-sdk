c08cf71a04b3076b9941ba96ae500764b3c728f7
周卓 <yecan.xyc@alibaba-inc.com>
2016-09-15 20:48:23 +0800

    为了尽量避免误判，提前5分钟认为sts token过期

--- oss-android-sdk/src/main/java/com/alibaba/sdk/android/oss/common/auth/OSSFederationCredentialProvider.java
+++ oss-android-sdk/src/main/java/com/alibaba/sdk/android/oss/common/auth/OSSFederationCredentialProvider.java
@@ -17,8 +17,10 @@ public abstract class OSSFederationCredentialProvider extends OSSCredentialProvi
     public abstract OSSFederationToken getFederationToken();
 
     public synchronized OSSFederationToken getValidFederationToken() {
+        // 判断sts token是否过期时，为了尽量避免误判，这里提前5分钟认为token过期
+        // 申请sts的最小有效期为15分钟
         if (cachedToken == null
-                || DateUtil.getFixedSkewedTimeMillis() / 1000 > cachedToken.getExpiration() - 15) {
+                || DateUtil.getFixedSkewedTimeMillis() / 1000 > cachedToken.getExpiration() - 5 * 60) {
 
             if (cachedToken != null) {
                 OSSLog.logD("token expired! current time: " + DateUtil.getFixedSkewedTimeMillis() / 1000 + " token expired: " + cachedToken.getExpiration());
