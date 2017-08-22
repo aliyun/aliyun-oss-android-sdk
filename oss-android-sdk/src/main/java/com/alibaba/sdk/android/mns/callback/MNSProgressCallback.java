f541adcd4db299619ab710e0ab053841a42e3fe7
zuoqin <zuoqin.wz@alibaba-inc.com>
2017-08-22 14:52:18 +0800

    Log: 移除mns 相关代码

--- oss-android-sdk/src/main/java/com/alibaba/sdk/android/mns/callback/MNSProgressCallback.java
+++
@@ -1,8 +0,0 @@
-package com.alibaba.sdk.android.mns.callback;
-
-/**
- * Created by pan.zengp on 2016/7/4.
- */
-public interface MNSProgressCallback<T> {
-    public void onProgress(T request, long currentSize, long totalSize);
-}
