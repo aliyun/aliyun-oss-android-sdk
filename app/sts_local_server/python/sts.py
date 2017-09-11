d8fd27dcb87ebada25d50d666b78b1e746c3d745
jingdan <haibing.whb@alibaba-inc.com>
2017-09-11 13:59:51 +0800

    add 调整python 的 注释 和  使用说明步骤

--- app/sts_local_server/python/sts.py
+++ app/sts_local_server/python/sts.py
@@ -6,7 +6,12 @@ from aliyunsdksts.request.v20150401 import AssumeRoleRequest
 
 def getSts():
 
-	# 通过管理控制后台-访问控制
+	# 通过管理控制后台-访问控制 https://help.aliyun.com/product/28625.html?spm=5176.doc28649.3.1.gXMMoS
+	# RAM用户创建步骤
+	# 1.创建RAM用户 https://help.aliyun.com/document_detail/28637.html?spm=5176.product28625.4.1.loOt1V
+	# 2.创建自定义授权策略 https://help.aliyun.com/document_detail/28640.html?spm=5176.product28625.4.2.QCYMTW
+	# 3.给RAM用户授权 https://help.aliyun.com/document_detail/28639.html?spm=5176.product28625.4.3.4ji7k1
+	# 4.RAM用户登录控制台 https://help.aliyun.com/document_detail/43640.html?spm=5176.product28625.4.4.0dU4bF
 	# 只有 RAM用户（子账号）才能调用 AssumeRole 接口
 	# 阿里云主账号的AccessKeys不能用于发起AssumeRole请求
 	# 请首先在RAM控制台创建一个RAM用户，并为这个用户创建AccessKeys
@@ -15,9 +20,9 @@ def getSts():
 	# 构建Aliyun Client时需要设置AccessKeyId和AccessKeySevcret
 	# STS是Global Service, API入口位于华东 1 (杭州) , 这里Region填写"cn-hangzhou"
 	# clt = client.AcsClient('<access-key-id>','<access-key-secret>','cn-hangzhou')
-	ak = "LTAIbPEsK8dzwHAy"
-	sk = "xudmIHvuehyiwdAwWQ93WYZIX9N2Qh"
-	role = "acs:ram::1855202418087745:role/role01"
+	ak = "***************************"
+	sk = "***************************"
+	role = "***************************"
 
 	clt = client.AcsClient(ak,sk,'cn-hangzhou')
 	# 构造"AssumeRole"请求
@@ -39,6 +44,6 @@ def getSts():
 	# 发起请求，并得到response
 	response = clt.do_action_with_exception(request)
 
-	# print response
+    print response
 	return response
 	pass
