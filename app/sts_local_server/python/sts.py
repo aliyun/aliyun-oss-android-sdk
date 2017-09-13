6b95d747f15a6ba5758905ff3a5f97b978dcece0
jingdan <haibing.whb@alibaba-inc.com>
2017-09-13 11:34:37 +0800

    fixed sts directions

--- app/sts_local_server/python/sts.py
+++ app/sts_local_server/python/sts.py
@@ -6,30 +6,30 @@ from aliyunsdksts.request.v20150401 import AssumeRoleRequest
 
 def getSts():
 
+
 	# 通过管理控制后台-访问控制 https://help.aliyun.com/product/28625.html
-	# 子用户(User)创建步骤
-	# 1.创建子用户(User) https://help.aliyun.com/document_detail/28637.html
-	# 2.创建自定义授权策略 https://help.aliyun.com/document_detail/28640.html
-	# 3.给子用户(User)授权 https://help.aliyun.com/document_detail/28639.html
-	# 4.子用户(User)登录控制台 https://help.aliyun.com/document_detail/43640.html
+	# RAM控制台 https://ram.console.aliyun.com/
+	# STS授权相关信息获取步骤:
+	# 1.RAM控制台用户管理创建子用户(User)同时点击该用户创建并获取AccessKeyID和AccessKeySecret https://help.aliyun.com/document_detail/28637.html
+	# 2.对该子用户(User) 授予AliyunSTSAssumeRoleAccess策略(必须),如需自定义策略请看 https://help.aliyun.com/document_detail/28640.html
+	# 3.RAM控制台角色管理创建角色role,进行自定义授权设置(控制操作的内容),获取Arn https://help.aliyun.com/document_detail/28649.html
+	# 注意点：
 	# 只有子用户(User)才能调用 AssumeRole 接口
 	# 阿里云主用户(Root User)的AccessKeys不能用于发起AssumeRole请求
-	# 请首先在RAM控制台创建一个子用户(User)，并为这个用户创建AccessKeys
-    # 创建 User/Role可以在主用户的控制台上一起完成的
-	# 对子用户(User)需要设置AliyunSTSAssumeRoleAccess权限
+	# python sdk说明
 	# 构建一个 Aliyun Client, 用于发起请求
 	# 构建Aliyun Client时需要设置AccessKeyId和AccessKeySevcret
 	# STS是Global Service, API入口位于华东 1 (杭州) , 这里Region填写"cn-hangzhou"
 	# clt = client.AcsClient('<access-key-id>','<access-key-secret>','cn-hangzhou')
-	ak = "***************************"
-	sk = "***************************"
-	role = "***************************"
+	AccessKeyID = "***************************"
+	AccessKeySecret = "***************************"
+	roleArn = "***************************"
 
-	clt = client.AcsClient(ak,sk,'cn-hangzhou')
+	clt = client.AcsClient(AccessKeyID,AccessKeySecret,'cn-hangzhou')
 	# 构造"AssumeRole"请求
 	request = AssumeRoleRequest.AssumeRoleRequest()
 	# 指定角色 需要在 RAM 控制台上获取
-	request.set_RoleArn(role)
+	request.set_RoleArn(roleArn)
 	# RoleSessionName 是临时Token的会话名称，自己指定用于标识你的用户，主要用于审计，或者用于区分Token颁发给谁
 	# 但是注意RoleSessionName的长度和规则，不要有空格，只能有'-' '.' '@' 字母和数字等字符
 	# 具体规则请参考API文档中的格式要求
