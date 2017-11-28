8e86862158b527c9590237e253e428b638994c59
jingdan <haibing.whb@alibaba-inc.com>
2017-11-28 12:04:36 +0800

    crc64

---
+++ oss-android-sdk/src/main/java/com/alibaba/sdk/android/oss/internal/HttpMessage.java
@@ -0,0 +1,58 @@
+package com.alibaba.sdk.android.oss.internal;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jingdan on 2017/11/27.
+ */
+
+abstract class HttpMessage {
+    private Map<String, String> headers = new HashMap<String, String>();
+    private InputStream content;
+    private long contentLength;
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        if (this.headers == null) {
+            this.headers = new HashMap<String, String>();
+        }
+        if (this.headers != null && this.headers.size() > 0){
+            this.headers.clear();
+        }
+
+        this.headers.putAll(headers);
+    }
+
+    public void addHeader(String key, String value) {
+        this.headers.put(key, value);
+    }
+
+    public InputStream getContent() {
+        return content;
+    }
+
+    public void setContent(InputStream content) {
+        this.content = content;
+    }
+
+    public long getContentLength() {
+        return contentLength;
+    }
+
+    public void setContentLength(long contentLength) {
+        this.contentLength = contentLength;
+    }
+
+    public void close() throws IOException {
+        if (content != null) {
+            content.close();
+            content = null;
+        }
+    }
+}
