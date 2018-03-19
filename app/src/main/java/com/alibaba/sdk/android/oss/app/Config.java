c10a1a8758d5d34f8e862678f9d6af1ef840b822
1019272778 <1019272778@qq.com>
2018-03-19 15:09:45 +0800

    change config para

--- app/src/main/java/com/alibaba/sdk/android/oss/app/Config.java
+++ app/src/main/java/com/alibaba/sdk/android/oss/app/Config.java
@@ -19,10 +19,10 @@ public class Config {
     public static final String callbackAddress = "http://oss-demo.aliyuncs.com:23450";
     // STS 鉴权服务器地址，使用前请参照文档 https://help.aliyun.com/document_detail/31920.html 介绍配置STS 鉴权服务器地址。
     // 或者根据工程sts_local_server目录中本地鉴权服务脚本代码启动本地STS 鉴权服务器。详情参见sts_local_server 中的脚本内容。
-    public static final String STSSERVER = "http://30.40.38.126:5566/sts/getsts";//STS 地址
+    public static final String STSSERVER = "http://*.*.*.*:****/sts/getsts";//STS 地址
 
     public static final String uploadFilePath = ""; //本地文件上传地址
-    public static final String bucket = "zuoqin-photoes";
+    public static final String bucket = "测试bucket名称";
     public static final String uploadObject = "上传object名称";
     public static final String downloadObject = "下载object名称";
 
