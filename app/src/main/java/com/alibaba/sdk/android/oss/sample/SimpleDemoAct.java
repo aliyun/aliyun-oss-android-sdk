2234cc228de398f3e58257fbbf43d92a1dcb744e
1019272778 <1019272778@qq.com>
2018-06-07 20:44:32 +0800

    Log :  add simple demo

---
+++ app/src/main/java/com/alibaba/sdk/android/oss/sample/SimpleDemoAct.java
@@ -0,0 +1,15 @@
+package com.alibaba.sdk.android.oss.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alibaba.sdk.android.oss.app.R;
+
+public class SimpleDemoAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_demo);
+    }
+}
