71c44a521047f4d715eb92bf3b2c7a5b61800081
润生 <sdlcdhch@gmail.com>
2018-09-11 18:42:47 +0800

    release 2.9.1

--- oss-android-sdk/src/main/java/com/alibaba/sdk/android/oss/common/OSSConstants.java
+++ oss-android-sdk/src/main/java/com/alibaba/sdk/android/oss/common/OSSConstants.java
@@ -5,7 +5,7 @@ package com.alibaba.sdk.android.oss.common;
  */
 public final class OSSConstants {
 
-    public static final String SDK_VERSION = "2.9.0";
+    public static final String SDK_VERSION = "2.9.1";
     public static final String DEFAULT_OSS_ENDPOINT = "http://oss-cn-hangzhou.aliyuncs.com";
 
     public static final String DEFAULT_CHARSET_NAME = "utf-8";
